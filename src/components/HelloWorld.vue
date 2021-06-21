0885dc215096fe76b92a6c70d39f8669eca57ff9
Icrani <Icrani335012@163.com>
2021-06-21 20:44:39 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="msg">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {//配置对象(与vue一致)
+  data () {
+    return {
+      msg: 'Hello Vue Component'
+    }
+  }
+}
+</script>
+
+<style>
+.msg {
+  color: red;
+  font-size: 30px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+/*
+入口JS：创建Vue实例
+* */
+
+import Vue from 'vue'
+import App from './App'
+
+
+new Vue({
+  el:'#app',
+  components:{
+    App
+  },
+  template:'<App/>'
+})
