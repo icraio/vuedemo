5f0aea5ee822d03ec02fb5ffc7578223e966cf3d
Icrani <Icrani335012@163.com>
2021-06-22 23:35:25 +0800

    update

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="col-md-4">
-   <div>
-     <label for="username">
-       <b>用户名</b>
-     </label>
-     <input type="text" id="username" placeholder="用户名" style="width: 353px">
-   </div>
-    <div>
-      <label for="cont"><b>评论内容</b></label><br>
-      <textarea name="" id="cont" cols="46" rows="5"></textarea>
-    </div>
-
-    <input type="submit" value="提交" style="float: right">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label for="username">
+          <b>用户名</b>
+        </label>
+        <input type="text" id="username" class="form-control" placeholder="用户名" style="width: 353px">
+      </div>
+      <div class="form-group">
+        <label for="cont"><b>评论内容</b></label><br>
+        <textarea class="form-control" id="cont" cols="46" rows="6"></textarea>
+      </div>
+      <div class="form-group">
+        <div class="col-offset-2 col-sm-10">
+          <button type="button" class="btn btn-default float-right" >提交</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
