e982ad30b45e005c8459c96485853ad6a979955c
Icrani <Icrani335012@163.com>
2021-06-23 19:21:40 +0800

    update

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="col-md-8">
     <h3 class="reply">评论回复：</h3>
-    <!--    <h2>暂无评论，点击左侧添加评论！！！</h2>-->
+    <h2 v-show="comments.length===0">暂无评论，点击左侧添加评论！！！</h2>
     <ul class="list-group">
       <Item v-for="(comment, index) in comments" :key="index" :comment="comment"
-      :deleteComment="deleteComment" :index="index"/>
+            :deleteComment="deleteComment" :index="index"/>
     </ul>
   </div>
 </template>
@@ -14,7 +14,7 @@ import Item from './Item'
 
 export default {
   //声明接收属性：这个属性就会成为组件对象的属性
-  props: ['comments','deleteComment'],//这种方式只指定属性名
+  props: ['comments', 'deleteComment'],//这种方式只指定属性名
   components: {
     Item
   }
