9013317dc6e4a8782b998560b523862f961166b9
Icrani <Icrani335012@163.com>
2021-06-28 17:41:30 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+
+//声明使用插件
+Vue.use(VueResource) //内部会给vm对象和组件对象添加一个属性：$http
 
 //在main.js中引用的文件在任何组件中都有效
 new Vue({
