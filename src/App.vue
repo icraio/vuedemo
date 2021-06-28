d0604cb8ecb49502c51ccc1f63cd21a38d9a6281
Icrani <Icrani335012@163.com>
2021-06-28 22:42:30 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,32 @@
 <template>
-  <div>
-    <div v-if="!repoUrl">loading</div>
-    <div v-else>most star repo is <a :href="repoUrl">{{ repoName }}</a></div>
+  <div class="container">
+    <Search></Search>
+    <users-main></users-main>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
-  data () {
-    return {
-      repoUrl: '',
-      repoName: ''
-    }
-  },
-  mounted () {
-    //发送ajax请求获取数据
-    const url = `https://api.github.com/search/repositories?q=v&sort=stars`
-    // this.$http.get(url).then(
-    //   //成功
-    //   response => {
-    //     const result = response.data
-    //     //得到最受欢迎的repo
-    //     const mostRepo = result.items[0]
-    //     this.repoUrl = mostRepo.html_url
-    //     this.repoName = mostRepo.name
-    //   },
-    //   response => {
-    //     alert('请求失败')
-    //   }
-    // )
-
-    //使用axios发送ajax请求
-    axios.get(url).then(response => {
-      //成功
-      const result = response.data
-      const mostRepo = result.items[0]
-      this.repoUrl = mostRepo.html_url
-      this.repoName = mostRepo.name
-    }).catch(error =>{
-      alert('请求失败')
-    })
-  }
+  name: 'App'
 }
-
 </script>
 
-<style>
+<style scoped>
+.card {
+  float: left;
+  width: 33.333%;
+  padding: .75rem;
+  margin-bottom: 2rem;
+  border: 1px solid #efefef;
+  text-align: center;
+}
 
+.card > img {
+  margin-bottom: .75rem;
+  border-radius: 100px;
+}
+
+.card-text {
+  font-size: 85%;
+}
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <h2>输入用户名搜索</h2>
+  <h2>LOADING...</h2>
+  <h2></h2>
+  <div class="row">
+    <div class="card">
+      <a href="https://github.com/reactjs" target="_blank">
+        <img :src="user.avatar_url" style="width: 100px;">
+      </a>
+      <p class="card-text">{{user.name}}</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style scoped>
+
+</style>
