29fc16af88c098131033161a7e3ef2395820e0f3
Icrani <Icrani335012@163.com>
2021-07-01 20:29:12 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
 <div>
   <div class="row">
+    <div class="col-xs-offset-2 col-xs-8">
+      <div class="page-header">
+        <h2>Router Basic - 01</h2>
+      </div>
+    </div>
+  </div>
 
+  <div class="row">
+    <div class="col-xs-2 col-xs-offset-2">
+      <div class="list-group">
+        <a href="#/about" class="list-group-item router-link-exact-active active"></a>
+      </div>
+    </div>
   </div>
 </div>
 </template>
