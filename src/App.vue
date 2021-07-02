152a0076ec2d6ba6528cbd9841e1d709f9abdcde
Icrani <Icrani335012@163.com>
2021-07-02 20:53:34 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-xs-offset-2 col-xs-8">
+      <div class="col-md-offset-8 col-xs-8">
         <div class="page-header"><h2>Router Basic - 01</h2></div>
       </div>
     </div>
@@ -10,7 +10,7 @@
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
           <router-link to="/about" class="list-group-item">About</router-link>
-          <router-link class="list-group-item">Home</router-link>
+          <router-link to="/home" class="list-group-item">Home</router-link>
         </div>
       </div>
       <div class="col-xs-6">
