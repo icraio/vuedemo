579afe2ce5a8a3f724dac846c00c64228a938324
Icrani <Icrani335012@163.com>
2021-07-08 20:03:00 +0800

    update

---
+++ src-sum/components/Count.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>当前求和为{{ sum }}</h2>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>&nbsp;
+    <button @click="increment">+</button>&nbsp;
+    <button @click="decrement">-</button>&nbsp;
+    <button @click="incrementOdd">奇数再加</button>&nbsp;
+    <button @click="incrementAsync">异步加</button>&nbsp;
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Count',
+  data () {
+    return {
+      sum: 0, //和
+      n: 1 //用户所选择的数字
+    }
+  },
+  methods: {
+    increment () {
+      this.sum += this.n
+    },
+    decrement () {
+      this.sum -= this.n
+    },
+    incrementOdd () {
+      if (this.sum % 2) {
+        this.sum+=this.n
+      }
+    },
+    incrementAsync () {
+      setTimeout(()=>{
+        this.sum+=this.n
+      },500)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src-sum/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+
+
+//在main.js中引用的文件在任何组件中都有效
+
+const vm = new Vue({//配置对象的属性名都是一些确定的名称，不能随便修改
+  el: '#app',
+  components: {App},
+  template: '<App/>',
+})
+
